a76c38e57f14b9cb15dcd59410a208a5305cd2fc
vinMoreiraHenrique <viniciusmoreirahenrique@gmail.com>
2023-01-22 00:51:01 -0300

    feat: render api data created and starting css

--- calculadora-de-antecipacao/src/components/dashboard/index.tsx
+++ calculadora-de-antecipacao/src/components/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import Api from "../../services/Api";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { Div } from "./styles";
@@ -11,61 +11,104 @@ interface CalculateData {
   days?: Array<number>;
 }
 
-interface CalculateDataRendering {
-  data: CalculateData[];
+interface IResult {
+  [key: string]: number;
 }
 
 function Dashboard() {
-  const [result, setResult] = useState<CalculateData[]>([]);
+  const [result, setResult] = useState<any>([]);
+
   const {
     register,
     handleSubmit,
     setValue,
+    watch,
     formState: { errors },
   } = useForm<CalculateData>();
 
-  const handleChange = (e: any) => {
+  const handleChange = ({ e }: any) => {
     setValue("days", e.target.value.split(",").map(Number));
   };
 
   function calculate(data: CalculateData) {
-    Api.post("/", data, 
-    {timeout: 5000}).then((response) => {
-      console.log(response.data);
-      
+    Api.post("/", data, { timeout: 5000 }).then((response) => {
+      setResult(response.data);
     });
   }
+
+  function renderResult(result: any) {
+    const resultArray: any = [];
+
+    for (const key in result) {
+      resultArray.push({ [key]: result[key] });
+    }
+
+    return resultArray.map((a: number, b: number) => {
+      let key = Object.keys(a)[0];
+      let value = Object.values(a)[0];
+      console.log(key);
+      if (key != "1") {
+        return <p key={b}>{`Em ${key} dias: ${value}`}</p>;
+      } else {
+        return <p key={b}>{`Amanhã: ${value}`}</p>;
+      }
+    });
+  }
+
   return (
     <Div>
-      <form onSubmit={handleSubmit(calculate)}>
-        <input
-          type="number"
-          placeholder="Digite o valor aqui..."
-          required
-          {...register("amount")}
-        />
-        <input
-          type="number"
-          placeholder="Digite o número de parcelas aqui..."
-          required
-          {...register("installments")}
-        />
-        <input
-          type="number"
-          placeholder="Digite a taxa MDR..."
-          required
-          {...register("mdr")}
-        />
-        <input
-          type="text"
-          placeholder="Digite os dias aqui..."
-          onChange={e => {
-            setValue("days", e.target.value.split(",").map(Number));
-          }}
-          required={false}
-        />
-        <button type="submit">Adicionar</button>
-      </form>
+      <div className="modal-calculate">
+        <form onSubmit={handleSubmit(calculate)}>
+          <h1>Simule sua antecipação</h1>
+
+          <input
+            type="number"
+            placeholder="Digite o valor aqui..."
+            required
+            {...register("amount")}
+          />
+          <input
+            type="number"
+            placeholder="Digite o número de parcelas aqui..."
+            required
+            {...register("installments")}
+          />
+          <input
+            type="number"
+            placeholder="Digite a taxa MDR..."
+            required
+            {...register("mdr")}
+          />
+          <input
+            type="text"
+            placeholder="Digite os dias aqui..."
+            required={false}
+            onChange={(e) =>
+              setValue("days", e.target.value.split(",").map(Number))
+            }
+            // {...register("days", {required: false,
+            //   onChange: (e) => {setValue("days", e.target.value.split(",").map(Number))},
+            // })}
+          />
+          <button
+            type="submit"
+            onClick={() => {
+              const resultArray = [];
+
+              for (const key in result) {
+                resultArray.push({ key: result[key] });
+              }
+              setResult(resultArray);
+            }}
+          >
+            Calcular
+          </button>
+        </form>
+        <aside>
+          <h2>Você receberá:</h2>
+          {result && renderResult(result)}
+        </aside>
+      </div>
     </Div>
   );
 }
--- calculadora-de-antecipacao/src/components/dashboard/styles.ts
+++ calculadora-de-antecipacao/src/components/dashboard/styles.ts
@@ -1,6 +1,41 @@
 import styled from "styled-components";
 
 export const Div = styled.div`
-display: flex;
-
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  .modal-calculate {
+    background-color: blue;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    
+    height: 60%;
+    width: 60%;
+    form {
+        background-color: yellow;
+      height: 100%;
+      width: 70%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      gap: 5px;
+      input {
+        height: 5%;
+      }
+    }
+    aside{
+        display: flex;
+        justify-content: space-around;
+        align-items: flex-start;
+        background-color: yellow;
+        height: 100%;
+        width: 30%;
+        background-color: orange;
+    }
+  }
 `;
